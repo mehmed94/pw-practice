9b2a8f37a18ced03d6fac62388c3e4e22eb3f1bd
mehmed94 <mehmed94@hotmail.com>
2024-09-03 11:29:56 +0200

    web server timeout update

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,8 @@ export default defineConfig<TestOptions>({
   ],
   webServer: {
     command: 'npm start',
-    url: 'http://localhost:4200/'
+    url: 'http://localhost:4200/',
+    timeout: 120 * 1000,
   }
 
 });
